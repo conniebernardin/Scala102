030eeaebbb69307d35b8fe5a44b0bc0e9099a3cb
Connie Bernardin <conniebernardin@googlemail.com>
2022-05-09 11:38:02 +0100

    function to calculate area of circle

--- src/main/scala/Practicals/CirclesFunction.scala
+++ src/main/scala/Practicals/CirclesFunction.scala
@@ -6,9 +6,16 @@ object CirclesFunction extends App {
 
   val pi = 3.14
 
+  val areaCalculator: (Double) => Double = (radius) =>
+    pi * (radius * radius)
+
   val circumferenceCalculator: (Double) => Double = (radius) =>
-    2 * pi * radius
+    pi * radius * 2
+
+  println(areaCalculator(2.5))
+  println(circumferenceCalculator(2.5))
 
 
-  println(circumferenceCalculator(2.0))
+  
+
 }
