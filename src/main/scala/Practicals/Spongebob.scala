3e4a656da2437c2d741c48bbce92937bfb554b50
Connie Bernardin <conniebernardin@googlemail.com>
2022-05-09 14:04:55 +0100

    checking spongbob function works

--- src/main/scala/Practicals/Spongebob.scala
+++ src/main/scala/Practicals/Spongebob.scala
@@ -3,7 +3,7 @@ package Practicals
 object Spongebob extends App {
 
   val spongebobQuote: (Option[String]) => String = character => character match {
-          case Some("spongebob") => " Excuse me, sir, but you’re sitting on my body, which is also my face."
+          case Some("spongebob") => "Excuse me, sir, but you’re sitting on my body, which is also my face."
           case Some("squidward") => "Too bad SpongeBob’s not here to enjoy Spongebob not being here"
           case Some("patrick") => "The inner machinations of my mind are an enigma"
           case Some("sandy") => "Stupidity isn't a virus, but it sure is spreading like one."
@@ -11,7 +11,9 @@ object Spongebob extends App {
           case _ => "Aye-aye, captain!"
         }
 
-  println(spongebobQuote(Some("sandh")))
+  println(spongebobQuote(Some("connie")))
+  println(spongebobQuote(Some("spongebob")))
+  println(spongebobQuote(Some("gary")))
 
 
   }
