612f3b834691911cfc5ad2160ea506c0bf6d1020
Connie Bernardin <conniebernardin@googlemail.com>
2022-05-10 13:35:47 +0100

    either excercise attempt 1

--- src/main/scala/Notes/Eithers.scala
+++ src/main/scala/Notes/Eithers.scala
@@ -6,15 +6,15 @@ object Eithers extends App {
   //commonly used for error handling where Left is the error case and right is the success case
   //good alternative to try catch if you want to do more than throw an error
 
-def getIntAndAdd1(s:String): Either[String, Int] = {
-  try {
-    Right(s.toInt)
-  } catch{
-    case e: Exception => Left("Failed")
+  def getIntAndAdd1(s: String): Either[String, Int] = {
+    try {
+      Right(s.toInt)
+    } catch {
+      case e: Exception => Left("Failed")
+    }
   }
-}
 
-/*  If you want to perform a function on an Either directly, you can first call
+  /*  If you want to perform a function on an Either directly, you can first call
    .isLeft or .isRight
    on the Either to find out which projection of the Either you are dealing with.
 
@@ -23,7 +23,7 @@ def getIntAndAdd1(s:String): Either[String, Int] = {
 
   println(getIntAndAdd1("12").right.map(num => num + 1))
   println(getIntAndAdd1("fail").left.map(word => word.toUpperCase()))
-  println(getIntAndAdd1("hello").right.map(num => num +1))
+  println(getIntAndAdd1("hello").right.map(num => num + 1))
 
 
   //You can avoid checking for isLeft or isRight by using pattern matching.
@@ -33,4 +33,20 @@ def getIntAndAdd1(s:String): Either[String, Int] = {
     case Left(_) => println("We have a left")
     case Right(_) => println("We have a right")
   }
+//
+//  def userExists(username: String, password: String): Boolean = {
+//    (username, password) match {
+//         case ("Connie Bernardin", "cutecats123") => true
+//    case ("Sarooon", "puppydog5") => true
+//    case ("Robyn", "MooseAndMaple") => true
+//    case _ => false
+//  }
+//}
+//
+//  def userEitherFunction(username: String, password: String): Either[Exception, Boolean] = {
+//    if (!userExists(username, password)) Left(Exception("UserNotFoundException"))
+//    else Right(userExists(username, password))
+//  }
+//
+//  println(userEitherFunction("lol", "123"))
 }
